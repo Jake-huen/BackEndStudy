7baa92e022c9d353e7c7d1ac7c536be55a17f379
username <57055730+Jake-huen@users.noreply.github.com>
2024-01-05 13:41:13 +0900

    HTTP 요청 메시지 - JSON

---
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,54 @@
+package hello.springmvc.basic.request;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class RequestBodyJsonController {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v2")
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws JsonProcessingException {
+        log.info("messageBody={}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        return "ok";
+    }
+
+    /**
+    * @RequestBody에 직접 만든 객체를 지정할 수 있다.
+     * HttpEntity<HelloData> 또는 @RequestBody 를 사용하면 `HTTP 메시지 컨버터`가 HTTP 메시지 바디의 내용을
+     * 우리가 원하는 문자나 객체 등으로 변환해준다.
+    * */
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> data) {
+        HelloData helloData = data.getBody();
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV4(@RequestBody HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return helloData;
+    }
+}
