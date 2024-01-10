2490fcd1b9795a5b03f0b06d6cb080a645f5d0a3
username <57055730+Jake-huen@users.noreply.github.com>
2024-01-10 14:39:09 +0900

    feat : Thymeleaf 프로젝트 생성

---
+++ ThymeleafBasicApplication/src/test/java/com/example/thymeleafbasicapplication/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafbasicapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
