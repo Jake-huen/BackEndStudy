e7df8c250024ce96248c1ab9e4f27f98241fa9a3
김태헌 <tae77777@naver.com>
2024-01-31 15:16:08 +0900

    FEAT : 메시지 국제화 ~ 4

--- spring-mvc-2/src/main/java/org/example/springmvc2/itemservice/ItemServiceApplication.java
+++ spring-mvc-2/src/main/java/org/example/springmvc2/itemservice/ItemServiceApplication.java
@@ -2,9 +2,6 @@ package org.example.springmvc2.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -12,13 +9,4 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
-
-	@Bean
-	public MessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasenames("messages", "errors");
-		messageSource.setDefaultEncoding("utf-8");
-		return messageSource;
-	}
-
 }
