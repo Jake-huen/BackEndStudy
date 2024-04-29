d2abfb1635872ba35f72d4e8c5a53048e56848f9
username <57055730+Jake-huen@users.noreply.github.com>
2024-04-29 19:53:55 +0900

    JpaShop change

--- jpashop2-api/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop2-api/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -13,10 +13,11 @@ public class JpashopApplication {
 	}
 
 	// Hibernate5 Library => 지연로딩 오류 방지
+	// 이런 방법이 있다는 걸 설명하는거지 실제로는 Entity 노출하면 안됨.
 	@Bean
 	Hibernate5Module hibernate5Module() {
 		Hibernate5Module hibernate5Module = new Hibernate5Module();
 		//강제 지연 로딩 설정
-		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		// hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
 		return hibernate5Module;
 	}}
--- jpashop2-api/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop2-api/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,6 +24,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() { // 엔티티를 지정 노출하면 문제생기는걸 보여주겠음..
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
         return all;
     }
 }
