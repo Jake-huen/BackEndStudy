c0f3a871aeef3221d711770db83732998ca79cd5
Jake-huen <tae77777@naver.com>
2024-04-30 17:47:09 +0900

    FEAT : Entity 반환 -> DTO 반환

--- jpashop2-api/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop2-api/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * xToOne ( Order와 Member는 ManyToOne)
@@ -29,5 +36,33 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // ORDER 2개
+        // N+1 문제 => 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        return orders.stream() // 결과적으로 2번 루프를 돌게 됨.
+                .map(SimpleOrderDto::new)
+                .collect(toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화 -> 영속성 컨텍스트가 멤버ID를 가지고, 영속성 컨텍스트를 찾아본다.
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
