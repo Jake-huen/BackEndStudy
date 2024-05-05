d85af358bdbf3414ba7faf7ef5ff37afc716dec4
username <57055730+Jake-huen@users.noreply.github.com>
2024-05-05 16:50:13 +0900

    FEAT : Class 클래스

---
+++ java-mid1/src/lang/clazz/Hello.java
@@ -0,0 +1,14 @@
+package lang.clazz;
+
+public class Hello {
+
+    private String value;
+
+    public String hello() {
+        return "hello!";
+    }
+
+    public Hello(String value) {
+        this.value = value;
+    }
+}
