f97fee3a33ebf542731cb0e8096051ac9cb449b7
Jake-huen <tae77777@naver.com>
2024-05-09 14:57:38 +0900

    FEAT : Enum 리팩토링

---
+++ java-mid1/src/enumeration/ref2/Grade.java
@@ -0,0 +1,23 @@
+package enumeration.ref2;
+
+public enum Grade {
+    BASIC(10), GOLD(20), DIAMOND(30);
+
+    private final int discountPercent;
+
+    Grade(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public int discount(int price) {
+        return price * discountPercent / 100;
+    }
+
+    public void printDiscount(Grade grade, int price) {
+        System.out.println(grade.name() + " 등급의 할인 금액: " + grade.discount(price));
+    }
+}
