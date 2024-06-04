5fa3f4a5b15ca0c923a076234e8ce98dae86bc9c
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-06-04 17:57:50 +0900

    Update CouponCreatedConsumer.java

--- coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
+++ coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
@@ -1,25 +1,31 @@
 package com.example.consumer.consumer;
 
 import com.example.consumer.domain.Coupon;
+import com.example.consumer.domain.FailedEvent;
 import com.example.consumer.repository.CouponRepository;
 import com.example.consumer.repository.FailedEventRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
     private final FailedEventRepository failedEventRepository;
 
-    public CouponCreatedConsumer(CouponRepository couponRepository, FailedEventRepository failedEventRepository) {
-        this.couponRepository = couponRepository;
-        this.failedEventRepository = failedEventRepository;
-    }
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
         // System.out.println(userId);
-        couponRepository.save(new Coupon(userId));
+        try {
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            log.error("failed to create coupon::" + userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
     }
 }
