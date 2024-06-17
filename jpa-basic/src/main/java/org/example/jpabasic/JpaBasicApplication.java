69a38dab3cd1f4158f08bd97aa8e8c5272286f3f
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-06-17 14:52:35 +0900

    JPA basic DB 설정

--- jpa-basic/src/main/java/org/example/jpabasic/JpaBasicApplication.java
+++ jpa-basic/src/main/java/org/example/jpabasic/JpaBasicApplication.java
@@ -1,12 +1,27 @@
 package org.example.jpabasic;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Member;
 
 @SpringBootApplication
 public class JpaBasicApplication {
 
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        tx.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
         SpringApplication.run(JpaBasicApplication.class, args);
     }
 
